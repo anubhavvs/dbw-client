a868be30dc5faf822fcd9df635491efe3c5ff6e6
Anubhav Saha <anubhavvsaha2001@gmail.com>
2023-06-29 23:21:10 +0200

    prod push

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 /* eslint-disable no-undef */
 export const URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : '';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000/api'
+    : 'https://dbw-server.onrender.com/api';
